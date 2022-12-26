232ebb035eccb227182c1f97e06f4ece23973e68
Apollostowel <apollostowel@icloud.com>
2022-12-26 23:54:08 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useAppDispatch, useAppSelector } from './hooks/reduxHook'
+import { createProduct, fetchAllProducts, sortByName } from './redux/reducers/productReducer'
 
 const App = () => {
+  const products = useAppSelector(state => state.productReducer)
+  const dispatch = useAppDispatch()
+
+  console.log(products)
+
+  const sortName = () => {
+    dispatch(sortByName("desc"))
+  }
+
+  const addProduct = () => {
+    dispatch(createProduct({
+      title: "New Test Product",
+      price: 10,
+      description: "A description",
+      categoryId: 1,
+      images: ["https://placeimg.com/640/480/any"],
+    }))
+  }
+
+  useEffect(() => {
+    dispatch(fetchAllProducts())
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      <button onClick={sortName}>Sort</button>
+      <button onClick={addProduct}>Add Test Product</button>
+      {products.map(product =>(<p key={product.id}>{product.title}</p>))}
+    </div>
   )
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 
+import productReducer from './reducers/productReducer';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    productReducer
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
