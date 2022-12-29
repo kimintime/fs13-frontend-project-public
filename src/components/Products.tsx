29726b7c0f9db4f48105ced377cc47f84e02bbb1
Apollostowel <apollostowel@icloud.com>
2022-12-29 15:14:16 +0200

    Continuing building  allProducts features.

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHook"
-import { createProduct, fetchAllProducts, sortByName } from "../redux/reducers/productReducer"
+import { createProduct, fetchAllProducts, sortByCatagory, sortByName, sortByPrice,  } from "../redux/reducers/productReducer"
 
 
 const Products = () => {
@@ -9,6 +9,14 @@ const Products = () => {
 
     const sortName = () => {
         dispatch(sortByName("desc"))
+    }
+
+    const sortPrice = () => {
+        dispatch(sortByPrice("desc"))
+    }
+
+    const sortCategory = () => {
+        dispatch(sortByCatagory("desc")) 
     }
 
     const addProduct = () => {
@@ -29,7 +37,9 @@ const Products = () => {
         <div>
             <button onClick={sortName}>Sort</button>
             <button onClick={addProduct}>Add Test Product</button>
-            {products.map(product => (<p key={product.id}>{product.title}</p>))}
+            <button onClick={sortPrice}>Sort Price</button>
+            <button onClick={sortCategory}>Sort Category</button>
+            {products.map(product => (<p key={product.id}>{product.title} {product.price} {product.category.name}</p>))}
         </div>
     )
 }
--- src/redux/reducers/productReducer.ts
+++ src/redux/reducers/productReducer.ts
@@ -42,13 +42,29 @@ const productSlice = createSlice({
             } else {
                 state.sort((a, b) => b.title.localeCompare(a.title))
             }
+        },
+        sortByPrice: (state, action: PayloadAction<"asc" | "desc">) => {
+            if (action.payload === "asc") {
+                state.sort((a, b) => a.price - b.price)
+
+            } else {
+                state.sort((a, b) => b.price - a.price)
+            }
+        },
+        sortByCatagory: (state, action: PayloadAction<"asc" | "desc">) => {
+            if (action.payload === "asc") {
+                state.sort((a, b) => a.category.name.localeCompare(b.category.name))
+
+            } else {
+                state.sort((a, b) => b.category.name.localeCompare(a.category.name))
+            }
         }
-        
     },
     extraReducers: (build) => {
         build.addCase(fetchAllProducts.fulfilled, (state, action) => {
             if (action.payload) {
                 return action.payload
+
             } else {
                 return state
             }
@@ -67,13 +83,20 @@ const productSlice = createSlice({
 
             } else {
                 return state
-            }
-            
+            } 
+        })
+        build.addCase(createProduct.rejected, (state, action) => {
+            console.log("Error posting new data")
+            return state
+        })
+        build.addCase(createProduct.pending, (state, action) => {
+            console.log("New data pending...")
+            return state
         })
     }
 })
 
 
 const productReducer = productSlice.reducer
-export const {sortByName} = productSlice.actions
+export const {sortByName, sortByPrice, sortByCatagory} = productSlice.actions
 export default productReducer;
