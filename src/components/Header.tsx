ba7f3c094b649f36baa4ea297a2d3e80b2eff9a2
Apollostowel <apollostowel@icloud.com>
2023-01-04 12:59:46 +0200

    Working on all products

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,20 @@ import { Link, useNavigate } from "react-router-dom"
 
 const Header = () => {
     const navigate = useNavigate()
+    const pages = [
+        {
+            nav: 'products',
+            name: 'Products'
+        },
+        {
+            nav: 'cart',
+            name: 'Shopping Cart',
+        },
+        {
+            nav: 'login',
+            name: 'Login'
+        },
+    ]
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -16,24 +30,22 @@ const Header = () => {
                         sx={{ mr: 2 }}
                     >
                     </IconButton>
-                    
-                    <Typography 
-                        variant="h4" 
-                        component="div" 
+                    <Typography
+                        variant="h4"
+                        component="div"
                         sx={{ flexGrow: 1 }}
-                        onClick={() => navigate("/") }
+                        onClick={() => navigate("/")}
                     >
                         Fake Web Store
                     </Typography>
-                    <Link to="products" style={{ textDecoration: 'none', color: 'white' }}>
-                        <Button color="inherit" variant="text">Products</Button>
-                    </Link>
-                    <Link to="cart" style={{ textDecoration: 'none', color: 'white' }}>
-                        <Button color="inherit" variant="text">Shopping Cart</Button>
-                    </Link>
-                    <Link to="login" style={{ textDecoration: 'none', color: 'white' }}>
-                        <Button color="inherit" variant="text">Login</Button>
-                    </Link>
+                    {
+                        pages.map(page => (
+                            <Link key={page.nav} to={page.nav} style={{ textDecoration: 'none', color: 'white' }}>
+                                <Button color="inherit" variant="text">{page.name}</Button>
+                            </Link>
+
+                        ))
+                    }
                 </Toolbar>
             </AppBar>
         </Box>
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,22 +1,44 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHook"
-import { createProduct, fetchAllProducts, sortByCatagory, sortByName, sortByPrice,  } from "../redux/reducers/productReducer"
+import { createProduct, fetchAllProducts, sortByCatagory, sortByName, sortByPrice, } from "../redux/reducers/productReducer"
+
+import { Box, Button, Divider, FormControl, InputLabel, MenuItem, Select } from "@mui/material"
 
 
 const Products = () => {
     const products = useAppSelector(state => state.productReducer)
     const dispatch = useAppDispatch()
 
+    const [type, setType] = useState('')
+
     const sortName = () => {
-        dispatch(sortByName("desc"))
+        if (type === "desc") {
+            dispatch(sortByName("desc"))
+
+        } else {
+
+            dispatch(sortByName("asc"))
+        }
     }
 
     const sortPrice = () => {
-        dispatch(sortByPrice("desc"))
+        if (type === "desc") {
+            dispatch(sortByPrice("desc"))
+
+        } else {
+
+            dispatch(sortByPrice("asc"))
+        }
     }
 
     const sortCategory = () => {
-        dispatch(sortByCatagory("desc")) 
+        if (type === "desc") {
+            dispatch(sortByCatagory("desc"))
+
+        } else {
+
+            dispatch(sortByCatagory("asc"))
+        }
     }
 
     const addProduct = () => {
@@ -34,13 +56,86 @@ const Products = () => {
     }, [])
 
     return (
-        <div>
-            <button onClick={sortName}>Sort</button>
-            <button onClick={addProduct}>Add Test Product</button>
-            <button onClick={sortPrice}>Sort Price</button>
-            <button onClick={sortCategory}>Sort Category</button>
+        <Box>
+            <Box style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center"
+            }}
+            >
+                <Button
+                    variant="contained"
+                    type="submit"
+                    size="large"
+                    onClick={sortName}
+                >
+                    Sort
+                </Button>
+                <FormControl sx={{ m: 1, minWidth: 80 }}>
+                    <InputLabel>Type</InputLabel>
+                    <Select
+                        value={type}
+                        label="Type"
+                        onChange={(event) => setType(event.target.value)}
+                    >
+                        <MenuItem value="asc">Ascending</MenuItem>
+                        <MenuItem value="desc">Descending</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button
+                    variant="contained"
+                    type="submit"
+                    size="large"
+                    onClick={sortPrice}
+                >
+                    Sort by Price
+                </Button>
+                <FormControl sx={{ m: 1, minWidth: 80 }}>
+                    <InputLabel>Type</InputLabel>
+                    <Select
+                        value={type}
+                        label="Type"
+                        onChange={(event) => setType(event.target.value)}
+                    >
+                        <MenuItem value="asc">Ascending</MenuItem>
+                        <MenuItem value="desc">Descending</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button
+                    variant="contained"
+                    type="submit"
+                    size="large"
+                    onClick={sortCategory}
+                >
+                    Sort by Category
+                </Button>
+                <FormControl sx={{ m: 1, minWidth: 80 }}>
+                    <InputLabel>Type</InputLabel>
+                    <Select
+                        value={type}
+                        label="Type"
+                        onChange={(event) => setType(event.target.value)}
+                    >
+                        <MenuItem value="asc">Ascending</MenuItem>
+                        <MenuItem value="desc">Descending</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button
+                    variant="contained"
+                    type="submit"
+                    size="large"
+                    onClick={addProduct}
+                >
+                    Add
+                </Button>
+            </Box>
+            <Divider variant="middle" />
+            {/* <Button onClick={sortName}>Sort</Button>
+            <Button onClick={addProduct}>Add Test Product</Button>
+            <Button onClick={sortPrice}>Sort Price</Button>
+            <Button onClick={sortCategory}>Sort Category</Button> */}
             {products.map(product => (<p key={product.id}>{product.title} {product.price} {product.category.name}</p>))}
-        </div>
+        </Box>
     )
 }
 
